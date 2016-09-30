cfec1ab145041e83818cc87e1df480be7b5db5ba
gaoyuyu <740514999@qq.com>
2016-09-30 15:19:20 +0800

    update

--- sample/src/main/java/com/gaoyy/sample/MainActivity.java
+++ sample/src/main/java/com/gaoyy/sample/MainActivity.java
@@ -1,10 +1,12 @@
-package com.gaoyy.circleindexview;
+package com.gaoyy.sample;
 
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
+
+import com.gaoyy.circleindexview.CircleIndexView;
 
 public class MainActivity extends AppCompatActivity
 {
